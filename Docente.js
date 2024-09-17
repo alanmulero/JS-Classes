34d14e46739534d7a0b1c5e4cf3dab735d9d5908
alanmulero <alan.mulero@gmail.com>
2024-09-17 17:08:46 -0300

    Testando encapsulamento

--- Docente.js
+++ Docente.js
@@ -1,21 +1,29 @@
 import User from "./User.js";
-class Docente extends User{
-    constructor(nome,email,role){
-        super(nome,email,role)
-    }
-
-    aprovaEstudante(nome,curso){
-
-        return `Estudante ${nome} passo no curso ${curso}, Responsável: ${this.nome}`;
+export default class Docente extends User {
+    constructor(nome, email, nascimento, role) {
+        super(nome, email, nascimento, role);
 
     }
+
+    aprovaEstudante(nome, curso) {
+
+        return `Estudante ${nome} passou no curso ${curso}, Responsável: ${this.nome}`;
+    }
+
 }
 
-    // Instanciando embaixo da classe.
-    // Preciso guardar a instancia em uma const.
-
-    const novoDocente = new Docente('Diloca','didi@email','professora');
-    console.log(novoDocente.aprovaEstudante('neny','miar'));
-    
 
 
+
+// Instanciando embaixo da classe.
+// Preciso guardar a instancia em uma const.
+
+const novoDocente = new Docente('Diloca', 'didi@email', 'professora');
+const docente2 = new Docente('Matilda', 'mt@email', 'diretora');
+console.log(novoDocente.exibirInfo());
+console.log(novoDocente);
+console.log(docente2);
+console.log(novoDocente.aprovaEstudante('neny', 'miar'));
+console.log(docente2.aprovaEstudante('mulerinho', 'Java'));
+const mudaNome = new User('Fernanda','fe@email','1995-05-03',);
+console.log(mudaNome);
